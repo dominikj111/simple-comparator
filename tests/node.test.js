ae740ff501033a39bde1e1390800a404da6ae37a
Dominik Jelinek <dominik.jelinek@any-3.com>
2023-10-25 11:35:14 +0200

    Edit readme file, tweak the node.js test file to confirm package.main path

--- tests/node.test.js
+++ tests/node.test.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { same } = require("../dist_commonjs");
+const { same } = require("..");
 
 test("`same` is available in Node.js runtime and works well", () => {
 	expect(same([1, { a: 1 }], [1, { a: 1 }])).toBe(true);
